72401ee84ead83d79c7de31521cf98e422a706d7
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2021-11-16 08:04:53 +0000

    Staging user routes

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const helmet = require("helmet")
 const morgan = require("morgan")
+const userRoute = require("./routes/user")
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true }, () => {
+//middleware
+app.use(express.json());
+app.use(helmet())
+app.use(morgan("common"))
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
     console.log("Connected Successfully to MongoDB")
-})
+});
 
 app.listen(8800, () => {
     console.log("Server running")
