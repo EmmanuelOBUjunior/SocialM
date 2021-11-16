3b7f84ce9251241034cb8efea12d1a2ef3884293
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2021-11-16 07:36:44 +0000

    Connecting to Database

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const helmet = require("helmet")
+const morgan = require("morgan")
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true }, () => {
+    console.log("Connected Successfully to MongoDB")
+})
+
+app.listen(8800, () => {
+    console.log("Server running")
+})
