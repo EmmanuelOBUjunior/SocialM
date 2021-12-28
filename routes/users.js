9224ced9c46d5fe01962aefd7c4b1446b4f6b70f
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2021-12-28 19:53:36 +0000

    Completing user.js file

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,11 @@
 const router = require("express").Router();
 
+
+//update a user
+//delete a user
+//get a user
+//follow a user
+//unfollow a user
 router.get("/", (req, res) => {
     res.send("This is user page")
 })
