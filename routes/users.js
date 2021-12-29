571521d5514a4a9035a40746770ae6861582289d
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2021-12-29 08:41:18 +0000

    get a user

--- routes/users.js
+++ routes/users.js
@@ -39,7 +39,17 @@ router.delete("/:id", async(req,res) =>{
 })
 
 //get a user
-router.get("/:id")
+router.get("/:id", async(req,res) =>{
+    try{
+            const user = await User.findById(req.params.id)
+            const {password,updatedAt, ...other} = user._doc;
+            res.status(200).json(other)
+        }catch(err){
+            res.status(400).json(err)
+        }
+
+    });
+
 //follow a user
 //unfollow a user
 
