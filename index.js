9722c9b004ab528f1daa6b7d871efc1090d69cf0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2021-12-30 07:11:15 +0000

    testing post.js route

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const helmet = require("helmet")
 const morgan = require("morgan")
 const userRoute = require("./routes/users")
 const authRoute = require('./routes/auth')
+const postsRoute = require('./routes/posts')
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.use(morgan("common"));
 
 app.use("/api/users", userRoute)
 app.use("/api/auth", authRoute)
+app.use("/api/posts", postsRoute)
 
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
