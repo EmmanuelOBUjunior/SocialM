e09db4f976d7d7864800bd5ddf8db428ce58e1b3
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2021-12-30 07:44:48 +0000

    update a post

--- routes/posts.js
+++ routes/posts.js
@@ -12,6 +12,19 @@ router.post("/", async(req,res) =>{
     }
 })
 //update a post
+router.put("/:id", async(req,res) =>{
+    try{
+    const post = await Post.findById(req.params.id)
+    if(post.userId === req.body.userId){
+        await post.updateOne({$set: req.body})
+        res.status(200).json("Post updated successfully")
+    }else{
+        res.status(503).json("You only update your post")
+    }
+    }catch(err){
+
+    }
+});
 //delete a post
 //like a post
 //get a post
