f937daeea4afa303aa8ad94a76426e0622f02557
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2021-12-30 07:05:31 +0000

    Added Post.js model

--- models/Post.js
+++ models/Post.js
@@ -2,8 +2,20 @@ const mongoose = require("mongoose")
 
 const PostSchema = new mongoose.Schema({
     userId:{
-        type: String
+        type: String,
+        required: true
     },
+    desc: {
+        type: String,
+        max: 500
+    },
+    likes: {
+        type: Array,
+        default: []
+    },
+    img: {
+        type: String
+    }
 },{ timestamps: true });
 
 
